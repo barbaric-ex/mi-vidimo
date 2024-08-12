cd4e070d9a7b789255b94ed156be403e9172dcab
barbaric-ex <v.barbaric@studio-exit.com>
2024-08-12 12:08:08 +0200

    footer počeo

--- footer.php
+++ footer.php
@@ -1,5 +1,29 @@
 </div>
 
+<div class="footer" id="footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-3">
+				<div class="logo_wrap">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/logo_main.png" alt="">
+				</div>
+			</div>
+
+			<div class="col-lg-3">
+				<div class="wrap wrap1">
+					<div class="title">
+						<h3>Adresa</h3>
+					</div>
+
+					<div class="text">
+						<p>Nadbiskupa Čule b.b.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
 
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.easing.min.js"></script>
@@ -9,16 +33,17 @@
 <?php wp_footer() ?>
 
 <script>
-    $(window).on('load', function () {
-	    wow = new WOW({
-	    boxClass: 'wow',
-	    animateClass: 'animated',
-	    offset: 5,
-	    mobile: true,
-	    live: true
-    })
-    wow.init();
-    });
+	$(window).on('load', function() {
+		wow = new WOW({
+			boxClass: 'wow',
+			animateClass: 'animated',
+			offset: 5,
+			mobile: true,
+			live: true
+		})
+		wow.init();
+	});
 </script>
 </body>
+
 </html>
