70aaba338a391fa72e986a9cf092c54005243dd1
barbaric-ex <v.barbaric@studio-exit.com>
2024-09-14 13:32:36 +0200

    14.9

--- page-home.php
+++ page-home.php
@@ -860,4 +860,195 @@ get_header(); ?>
     </div>
 </div>
 
+<!------------------------------------------------------------------------------>
+
+<div class="carousel courasel-cool">
+    <!-- list item -->
+    <div class="list">
+        <div class="item">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/po.jpg">
+            <div class="content">
+                <div class="author">LUNDEV</div>
+                <div class="title">DESIGN SLIDER</div>
+                <div class="topic">ANIMAL</div>
+
+                <div class="buttons">
+                    <button>SEE MORE</button>
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/s1.jpg">
+            <div class="content">
+                <div class="author">LUNDEV</div>
+                <div class="title">DESIGN SLIDER</div>
+                <div class="topic">ANIMAL</div>
+
+                <div class="buttons">
+                    <button>SEE MORE</button>
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/img3.jpg">
+            <div class="content">
+                <div class="author">LUNDEV</div>
+                <div class="title">DESIGN SLIDER</div>
+                <div class="topic">ANIMAL</div>
+
+                <div class="buttons">
+                    <button>SEE MORE</button>
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/img4.jpg">
+            <div class="content">
+                <div class="author">LUNDEV</div>
+                <div class="title">DESIGN SLIDER</div>
+                <div class="topic">ANIMAL</div>
+
+                <div class="buttons">
+                    <button>SEE MORE</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="item">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/img4.jpg">
+            <div class="content">
+                <div class="author">LUNDEV</div>
+                <div class="title">DESIGN SLIDER</div>
+                <div class="topic">ANIMAL</div>
+
+                <div class="buttons">
+                    <button>SEE MORE</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- list thumnail -->
+    <div class="thumbnail">
+        <div class="item">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/po.jpg">
+            <div class="content">
+                <div class="title">
+                    Name Slider
+                </div>
+                <div class="description">
+                    Description
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/s1.jpg">
+            <div class="content">
+                <div class="title">
+                    Name Slider
+                </div>
+                <div class="description">
+                    Description
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/img3.jpg">
+            <div class="content">
+                <div class="title">
+                    Name Slider
+                </div>
+                <div class="description">
+                    Description
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/img4.jpg">
+            <div class="content">
+                <div class="title">
+                    Name Slider
+                </div>
+                <div class="description">
+                    Description
+                </div>
+            </div>
+        </div>
+
+        <div class="item">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/img4.jpg">
+            <div class="content">
+                <div class="title">
+                    Name Slider
+                </div>
+                <div class="description">
+                    Description
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- next prev -->
+
+    <div class="arrows">
+        <button id="prev">
+            < </button>
+
+                <button id="next">></button>
+    </div>
+    <!-- time running -->
+    <div class="time"></div>
+</div>
+
+<script>
+    //step 1: get DOM
+    let nextDom = document.getElementById('next');
+    let prevDom = document.getElementById('prev');
+
+    let carouselDom = document.querySelector('.carousel');
+    let SliderDom = carouselDom.querySelector('.carousel .list');
+    let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
+    let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
+    let timeDom = document.querySelector('.carousel .time');
+
+    thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+    let timeRunning = 3000;
+    let timeAutoNext = 7000;
+
+    nextDom.onclick = function() {
+        showSlider('next');
+    }
+
+    prevDom.onclick = function() {
+        showSlider('prev');
+    }
+    let runTimeOut;
+    let runNextAuto = setTimeout(() => {
+        next.click();
+    }, timeAutoNext)
+
+    function showSlider(type) {
+        let SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
+        let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
+
+        if (type === 'next') {
+            SliderDom.appendChild(SliderItemsDom[0]);
+            thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+            carouselDom.classList.add('next');
+        } else {
+            SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
+            thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
+            carouselDom.classList.add('prev');
+        }
+        clearTimeout(runTimeOut);
+        runTimeOut = setTimeout(() => {
+            carouselDom.classList.remove('next');
+            carouselDom.classList.remove('prev');
+        }, timeRunning);
+
+        clearTimeout(runNextAuto);
+        runNextAuto = setTimeout(() => {
+            next.click();
+        }, timeAutoNext)
+    }
+</script>
+
 <?php get_footer(); ?>
